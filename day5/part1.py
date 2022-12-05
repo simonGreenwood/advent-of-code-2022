dcb4e6fd38586c6c20e9df8a8a5057bf430a8705
Simon Greenwood <simonrobertgreenwood@gmail.com>
2022-12-05 19:53:36 +0000

    commit

--- day5/part1.py
+++ day5/part1.py
@@ -1,6 +1,16 @@
-crates=[[],[],[]]
+columns = 3
+crates=[[] for x in range(columns)]
+start = True
 for line in open('example.txt').read().splitlines():
-  positions = list(range(1,34,4))
-  for i in range(0,3):
-    crates[i].append(line[positions[i]:positions[i]+4])
-print(list(map(lambda x: x.replace(" ","").replace("]","").replace("[",""),crates)))
+  if line[1::4] == "123":
+    start = False
+    crates = list(map(lambda c: c[::-1],crates))
+  if start:
+    for i in range(columns):
+      crates[i].append(line[1::4][i])
+      crates[i] = list(filter(lambda x: x != ' ',crates[i]))
+  if not start and not line[1::4] == "123" and line != "":
+    instruction = line.split(" ")
+    amount, s, f = int(instruction[1]), int(instruction[3]), int(instruction[5])
+    print(amount, s, f)
+print(crates)
