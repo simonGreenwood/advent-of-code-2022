613bb730a9a977408487d1ed9db22ce4c81da8cd
Simon Greenwood <simonrobertgreenwood@gmail.com>
2022-12-06 07:04:03 +0000

    day 5

--- day5/part1.py
+++ day5/part1.py
@@ -1,16 +1,18 @@
-columns = 3
+columns = 9
 crates=[[] for x in range(columns)]
 start = True
-for line in open('example.txt').read().splitlines():
-  if line[1::4] == "123":
+for line in open('input.txt').read().splitlines():
+  if line[1::4] == "123456789":
     start = False
     crates = list(map(lambda c: c[::-1],crates))
   if start:
     for i in range(columns):
       crates[i].append(line[1::4][i])
       crates[i] = list(filter(lambda x: x != ' ',crates[i]))
-  if not start and not line[1::4] == "123" and line != "":
+  if not start and not line[1::4] == "123456789" and line != "":
     instruction = line.split(" ")
     amount, s, f = int(instruction[1]), int(instruction[3]), int(instruction[5])
-    print(amount, s, f)
-print(crates)
+    for i in range(amount):
+      crates[f-1].append(crates[s-1].pop())
+    print(crates)
+print("".join([stack[-1] for stack in crates]))
